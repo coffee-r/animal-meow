ec75d3861cd694748bddd08ce755d7d52fc99e49
coffee-r <game.developer.ryo@gmail.com>
2022-08-08 22:10:36 +0900

    [add] Laravel Sanctum setting

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1','animal-meow.com','staging.animal-meow.com',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
