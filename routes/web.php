8392f96b8186e4917bc92055b26d92ab7392c9aa
coffee-r <game.developer.ryo@gmail.com>
2022-08-13 12:18:10 +0900

    [fix] ルーティング

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -15,13 +15,53 @@ use Inertia\Inertia;
 |
 */
 
-// トップ
-Route::get('/', 'App\Http\Controllers\IndexController@index')->name('index');
+// ゲストユーザー
+Route::middleware('guest')->group(function () {
+    // トップ画
+    Route::get('/', 'App\Http\Controllers\IndexController@index')->name('index');
+
+    // Twitterでログイン
+    Route::get('/login/twitter', 'App\Http\Controllers\TwitterLoginController@redirectToProvider')->name('twitter.login');
+
+    // Twitterログイン時のコールバック
+    Route::get('/login/twitter/callback', 'App\Http\Controllers\TwitterLoginController@handleProviderCallback');
+});
+
+// ログインユーザー
+Route::middleware('auth')->group(function () {
+    // ホーム
+    Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home');
+
+    // 自分の投稿一覧
+    Route::get('/me', 'App\Http\Controllers\HomeController@me')->name('me');
+
+    // 新規投稿
+    Route::get('/post', 'App\Http\Controllers\PostController@create')->name('post.create');
+
+    // 投稿処理
+    Route::post('/post', 'App\Http\Controllers\PostController@store')->name('post.store');
+
+    // その他
+    Route::get('/others', function () {
+        return Inertia::render('Others');
+    })->name('others');
+
+    // ログアウト処理
+    Route::post('/logout', function(){
+        Auth::logout();
+        return redirect(route('index'));
+    })->name('logout');
+
+    // 退会処理
+    Route::post('/withdrawal', 'App\Http\Controllers\WithdrawalController@withdrawal')->name('withdrawal');
+});
 
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// 退会後
+Route::get('/after_withdrawal', function () {
+    return Inertia::render('AfterWithdrawal');
+})->name('withdrawal.after');
+
 
 // 利用規約
 Route::get('/terms', function () {
