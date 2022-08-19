dba505467dc16d54d46ceaedc40f8a48f914b8c1
coffee-r <game.developer.ryo@gmail.com>
2022-08-19 17:03:35 +0900

    [add] 新規投稿機能

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,17 +6,12 @@ use Inertia\Inertia;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Http\Request;
 use App\Models\Post;
-use App\CoffeeR\Post\Domain\AnimalMessageFactory;
-use App\CoffeeR\Post\Domain\CatMessage;
-use App\CoffeeR\Post\Domain\DogMessage;
-use App\CoffeeR\Post\Domain\GoriraMessage;
-use App\CoffeeR\Post\Domain\ChickMessage;
-use App\CoffeeR\Post\Domain\ElephantMessage;
-use App\CoffeeR\Post\Domain\FlogMessage;
-use App\CoffeeR\Post\Repository\Tweet;
-use App\CoffeeR\Post\Repository\TweetRepository;
+use App\Models\Animal;
+use App\CoffeeR\UseCases\PostStoreAction;
+use App\CoffeeR\UseCases\TweetAction;
 use Exception;
 
 class PostController extends Controller
@@ -29,14 +24,7 @@ class PostController extends Controller
     public function create()
     {
         // 投稿に利用できる言葉を動物ごとに取得する
-        $animals = [
-            1 => ['id' => 1, 'name' => 'ねこ', 'availableWords' => CatMessage::AVAILABLE_WORDS],
-            2 => ['id' => 2, 'name' => 'いぬ', 'availableWords' => DogMessage::AVAILABLE_WORDS],
-            3 => ['id' => 3, 'name' => 'ごりら', 'availableWords' => GoriraMessage::AVAILABLE_WORDS],
-            4 => ['id' => 4, 'name' => 'ひよこ', 'availableWords' => ChickMessage::AVAILABLE_WORDS],
-            5 => ['id' => 5, 'name' => 'ぞう', 'availableWords' => ElephantMessage::AVAILABLE_WORDS],
-            6 => ['id' => 6, 'name' => 'かえる', 'availableWords' => FlogMessage::AVAILABLE_WORDS],
-        ];
+        $animals = Animal::all();
 
         return Inertia::render('Post', [
             'animals' => $animals,
@@ -49,47 +37,54 @@ class PostController extends Controller
      * @param Request $request
      * @return void
      */
-    public function store(Request $request)
+    public function store(Request $request, PostStoreAction $postStoreAction, TweetAction $tweetAction)
     {
         // フォームバリデーション
         $validated = $request->validate([
-            'animalTypeId' => 'required',
+            'animalId' => 'required',
             'message' => 'required|max:120',
         ],[
-            'animalTypeId.required' => '動物を選択してください',
+            'animalId.required' => '動物を選択してください',
             'message.required' => '投稿メッセージを入力してください',
             'message.max' => '投稿メッセージは最大120文字までです',
         ]);
 
         // フラッシュメッセージ用変数
         $successMessages = [];
-
-        // 値オブジェクトを通じて投稿メッセージをバリデーション
-        $animalMessageFactory = new AnimalMessageFactory();
-        $animalMessage = $animalMessageFactory->create($request->input('animalTypeId'), $request->input('message'));
+        $failMessages = [];
 
         // トランザクション
+        // このサービスの投稿もツイートも成功 ⇨ コミット
+        // このサービスの投稿が失敗 ⇨ ロールバック
+        // このサービスの投稿は成功してツイートが失敗 ⇨ ロールバック
         DB::beginTransaction();
 
-        // 投稿
-        $post = new Post();
-        $post->user_id = Auth::id();
-        $post->animal_type_id = $request->input('animalTypeId');
-        $post->message = $animalMessage->toString();
-        $post->save();
-        $successMessages[] = "投稿しました。";
+        try{
+            // 新規投稿処理
+            $postStoreAction($request->input('animalId'), $request->input('message'));
+            $successMessages[] = "投稿しました。";
 
-        // ツイート
-        if ($request->input('withTweet')) {
-            $tweetRepository = new TweetRepository();
-            $tweet = $tweetRepository->tweet($animalMessage->toString() . " #あにまるにゃ〜ん");
-            $successMessages[] = "<a class='underline' href='".$tweet->url."' target='_blank'>Twitter</a>に投稿しました。";
+            // ツイート
+            if ($request->input('withTweet')) {
+                $tweet = $tweetAction($request->input('message'));
+                $successMessages[] = "<a class='underline' href='".$tweet->url."' target='_blank'>Twitter</a>に投稿しました。";
+            }
+        }catch(Exception $e){
+            // エラーログ書き込み
+            Log::error($e->getMessage());
+
+            // フラッシュメッセージ設定
+            $failMessages[] = "投稿に失敗しました。";
+            var_dump($e->getMessage());exit();
+            
+            // ホーム画面にリダイレクト
+            return redirect('/home')->with('failMessages', $failMessages);
         }
 
         // コミット
         DB::commit();
 
-        // 削除元の画面にリダイレクト
+        // ホーム画面にリダイレクト
         return redirect('/home')->with('successMessages', $successMessages);
     }
 
