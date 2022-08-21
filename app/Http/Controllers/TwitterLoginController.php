1d880b53964b536455135cbc91df53600b876ea7
coffee-r <game.developer.ryo@gmail.com>
2022-08-21 14:26:52 +0900

    [fix] 存在しないクラス削除

--- app/Http/Controllers/TwitterLoginController.php
+++ app/Http/Controllers/TwitterLoginController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\CoffeeR\UseCases\TwitterLoginAction;
 use App\CoffeeR\UseCases\UserUpsertWithTwitterAction;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Auth;
