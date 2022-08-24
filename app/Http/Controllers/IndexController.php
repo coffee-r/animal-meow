c78e0bf2fdad5ce2b4870cde24ff2936570167e1
coffee-r <game.developer.ryo@gmail.com>
2022-08-25 06:09:59 +0900

    [fix] TOPページ高速化

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Inertia\Inertia;
 use App\Models\Post;
+use Illuminate\Support\Facades\Cache;
 
 class IndexController extends Controller
 {
@@ -14,12 +15,14 @@ class IndexController extends Controller
      */
     public function index()
     {
-        // 最新の投稿を10件取得
-        $posts = Post::join('users', 'users.id', '=', 'posts.user_id')
-                     ->select(['posts.id as post_id', 'user_id', 'message', 'like_total_count', 'avatar_image_url', 'users.name as user_name', 'posts.created_at as post_created_at'])
-                     ->orderBy('posts.id', 'desc')
-                     ->take(10)
-                     ->get();
+        // 最新の投稿10件をキャッシュしつつ取得
+        $posts = Cache::remember('index_posts', 60, function(){
+            return Post::join('users', 'users.id', '=', 'posts.user_id')
+                        ->select(['posts.id as post_id', 'user_id', 'message', 'like_total_count', 'avatar_image_url', 'users.name as user_name', 'posts.created_at as post_created_at'])
+                        ->orderBy('posts.id', 'desc')
+                        ->take(10)
+                        ->get();
+        });
 
         return Inertia::render('Index', [
             'posts' => $posts,
